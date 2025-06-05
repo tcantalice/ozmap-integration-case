f609ca982fb497dc911c32bcff7eea9f10e27227
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-05 14:40:42 +0000

    feat(importer): Implementa requisição de dados via gateway HTTP (WIP)

--- src/importer/gateways/isp-service-http-gateway.ts
+++ src/importer/gateways/isp-service-http-gateway.ts
@@ -2,6 +2,18 @@ import { Axios } from 'axios';
 import ISPServiceGateway from '../contracts/isp-service-gateway';
 import ISPIntegrationData from '../isp-integration-data';
 
+class RequestError {
+  constructor(private previous: Error) {}
+}
+
+class ResponseError {
+  constructor(private previous: Error) {}
+}
+
+class ConnectionError {
+  constructor(private previous: Error) {}
+}
+
 export default class ISPServiceHttpGateway implements ISPServiceGateway {
   private __httpClient: Axios; // TODO: abstrair uso do Axios
 
@@ -11,12 +23,21 @@ export default class ISPServiceHttpGateway implements ISPServiceGateway {
     });
   }
 
-  getData(): ISPIntegrationData {
-    return {
-      boxes: [],
-      cables: [],
-      customers: [],
-      drop_cables: [],
-    };
+  async getData(): Promise<ISPIntegrationData> {
+    let data: ISPIntegrationData;
+
+    try {
+      data = await this.requestData();
+    } catch (err) {
+      throw err;
+    }
+
+    return data;
+  }
+
+  private async requestData(): Promise<ISPIntegrationData> {
+    const response = await this.__httpClient.get<ISPIntegrationData>('/');
+
+    return response.data;
   }
 }
