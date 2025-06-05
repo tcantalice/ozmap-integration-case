85e5397ea68621071c239e5989c62430a7b9fc4f
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-05 15:40:25 +0000

    refactor(importer/gateways): Refatora implementação do gateway baseado em http
    Implementa tratamento de erros. Remove assinaturas antigas de métodos da versão anterior do contrato.

--- src/importer/gateways/isp-service-http-gateway.ts
+++ src/importer/gateways/isp-service-http-gateway.ts
@@ -1,17 +1,51 @@
-import { Axios } from 'axios';
+import { Axios, AxiosResponse, isAxiosError } from 'axios';
+import { ClientRequest } from 'node:http';
 import ISPServiceGateway from '../contracts/isp-service-gateway';
-import ISPIntegrationData from '../isp-integration-data';
+import {
+  ISPBoxData,
+  ISPCableData,
+  ISPCustomerData,
+  ISPDropCableData,
+} from '../isp-integration-data';
 
-class RequestError {
-  constructor(private previous: Error) {}
+class RequestError extends Error {
+  constructor(
+    message: string,
+    cause: Error,
+    private requestData: object,
+  ) {
+    super(message, { cause });
+  }
+
+  getContext(): object {
+    return { request: this.requestData };
+  }
 }
 
-class ResponseError {
-  constructor(private previous: Error) {}
+class ResponseError extends Error {
+  constructor(
+    message: string,
+    cause: Error,
+    private statusHttp: number,
+    private responseData: object,
+    private requestData: object,
+  ) {
+    super(message, { cause });
+  }
+
+  getContext(): object {
+    return {
+      status: this.statusHttp,
+      response: this.responseData,
+      request: this.requestData,
+    };
+  }
 }
 
-class ConnectionError {
-  constructor(private previous: Error) {}
+class UnknowIntegrationError extends Error {
+  constructor(message: string, cause: Error) {
+    super(message, { cause });
+  }
 }
 
 export default class ISPServiceHttpGateway implements ISPServiceGateway {
@@ -23,21 +57,53 @@ export default class ISPServiceHttpGateway implements ISPServiceGateway {
     });
   }
 
-  async getData(): Promise<ISPIntegrationData> {
-    let data: ISPIntegrationData;
-
-    try {
-      data = await this.requestData();
-    } catch (err) {
-      throw err;
-    }
-
-    return data;
+  getBoxes(): Promise<ISPBoxData[]> {
+    return this.makeHttpRequest<ISPBoxData[]>('/boxes');
   }
 
-  private async requestData(): Promise<ISPIntegrationData> {
-    const response = await this.__httpClient.get<ISPIntegrationData>('/');
+  getCables(): Promise<ISPCableData[]> {
+    return this.makeHttpRequest<ISPCableData[]>('/cables');
+  }
 
-    return response.data;
+  getDropCables(): Promise<ISPDropCableData[]> {
+    return this.makeHttpRequest<ISPDropCableData[]>('/drop_cables');
+  }
+
+  getCustomers(): Promise<ISPCustomerData[]> {
+    return this.makeHttpRequest<ISPCustomerData[]>('/customers');
+  }
+
+  private async makeHttpRequest<ResponseData>(path: string): Promise<ResponseData> {
+    return this.__httpClient
+      .get<ResponseData>(path)
+      .then((response: AxiosResponse<ResponseData>) => response.data)
+      .catch((err: any) => this.handleError(err));
+  }
+
+  private handleError(err: any): never {
+    if (isAxiosError(err)) {
+      if (err.response) {
+        const response = err.response;
+        const request = err.request as ClientRequest;
+
+        throw new ResponseError(
+          'ISP data request not successful',
+          err,
+          response.status,
+          response.data as object,
+          { headers: request.getHeaders(), host: request.host, path: request.path },
+        );
+      } else if (err.request) {
+        const request = err.request as ClientRequest;
+
+        throw new RequestError('Attempt to request ISP failed', err, {
+          headers: request.getHeaders(),
+          host: request.host,
+          path: request.path,
+        });
+      }
+    }
+
+    throw new UnknowIntegrationError('An unknown error occurs during the integration attempt', err);
   }
 }
