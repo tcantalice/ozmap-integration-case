73ccaccdbe48755e89e2fef6845f6a4496225e8a
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-06 13:42:09 +0000

    feat(models): Implementa models de Pole (WIP)

--- src/models/box-model.ts
+++ src/models/box-model.ts
@@ -39,13 +39,45 @@ export class BoxType {
 }
 
 export class Pole {
-  ozmapId: string | null;
+  private __ozmapId: string | null;
+
+  private __synchronizedAt: Date | null;
 
   constructor(
     public readonly lat: number,
     public readonly lng: number,
   ) {
-    this.ozmapId = null;
+    this.__ozmapId = null;
+    this.__synchronizedAt = null;
+  }
+
+  synchronize(id: string): void;
+  synchronize(id: string, timestamp: Date): void;
+
+  synchronize(id: string, timestamp?: Date): void {
+    this.setOzmapId(id);
+
+    this.setSynchronizedAt(timestamp || new Date());
+  }
+
+  private setOzmapId(id: string) {
+    this.__ozmapId = this.__ozmapId || this.__ozmapId;
+  }
+
+  private setSynchronizedAt(timestamp: Date) {
+    this.__synchronizedAt = this.__synchronizedAt || timestamp;
+  }
+
+  isCreated(): boolean {
+    return Boolean(this.__ozmapId);
+  }
+
+  public get synchId(): string | null {
+    return this.__ozmapId;
+  }
+
+  public get syncrhonizedAt(): Date | null {
+    return this.__synchronizedAt;
   }
 }
 
