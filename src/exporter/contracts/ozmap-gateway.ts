3712ad2ea73b0541237badc3e563e464e52990e2
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-07 17:41:47 +0000

    feat(exporter/contracts): Adiciona método de cadastro de Property ao gateway

--- src/exporter/contracts/ozmap-gateway.ts
+++ src/exporter/contracts/ozmap-gateway.ts
@@ -1,5 +1,8 @@
 import { OZMapBoxInputData, OZMapBoxOutputData } from '../data/ozmap-box.data';
+import { OZMapPropertyInput, OZMapPropertyOutput } from '../data/ozmap-property.data';
 
 export default interface OZMapGateway {
   createBoxResource(data: OZMapBoxInputData): Promise<OZMapBoxOutputData>;
+
+  createProperty(data: OZMapPropertyInput): Promise<OZMapPropertyOutput>;
 }
