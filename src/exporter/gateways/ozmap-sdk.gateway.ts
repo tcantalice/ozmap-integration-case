0afa548717e70275ffed966475e4f3d7ed18f298
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-07 17:56:16 +0000

    feat(exporter/gateways): Implementa método de cadastro de Cable

--- src/exporter/gateways/ozmap-sdk.gateway.ts
+++ src/exporter/gateways/ozmap-sdk.gateway.ts
@@ -1,6 +1,7 @@
-import OZMapSDK, { Box as OZBox } from '@ozmap/ozmap-sdk';
+import OZMapSDK, { Box as OZBox, Cable as OZCable } from '@ozmap/ozmap-sdk';
 import OZMapGateway from '../contracts/ozmap-gateway';
 import { OZMapBoxInputData, OZMapBoxOutputData } from '../data/ozmap-box.data';
+import { OZMapCableInputData, OZMapCableOutputData } from '../data/ozmap-cable.data';
 
 export default class OZMapSDKGateway implements OZMapGateway {
   private readonly sdk: OZMapSDK;
@@ -22,5 +23,20 @@ export default class OZMapSDKGateway implements OZMapGateway {
     });
 
     return { id: createdBox.id as string };
+  }
+
+  async createCable(data: OZMapCableInputData): Promise<OZMapCableOutputData> {
+    const createdCable: OZCable = await this.sdk.cable.create({
+      cableType: data.type,
+      project: data.project,
+      implanted: data.implanted,
+      hierarchyLevel: data.hierarchyLevel,
+      poles: data.poles,
+      boxA: data.boxA,
+      boxB: data.boxB,
+      name: data.name,
+    });
+
+    return { id: createdCable.id as string };
   }
 }
