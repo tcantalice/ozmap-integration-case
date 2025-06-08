32b481236c9df75b007bca23fe1fea5c5caddb2c
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-08 11:48:50 +0000

    refactor(service): Instancia controller de integração

--- src/service.ts
+++ src/service.ts
@@ -1,18 +1,19 @@
 import { Config } from './config/types';
 import IntegrationController from './core/integration.controller';
+import ISPServiceHttpGateway from './importer/gateways/isp-service-http-gateway';
+import ISPImporter from './importer/isp-importer';
 import ObservabilityManager from './observability/manager';
 
 export default class Service {
   private readonly controller: IntegrationController;
 
-  constructor(
-    config: Config,
-    private readonly __observability: ObservabilityManager,
-  ) {
-    this.controller = new IntegrationController();
+  constructor(config: Config) {
+    const importer = new ISPImporter(new ISPServiceHttpGateway(config.importer.gateway.url));
+
+    this.controller = new IntegrationController(importer);
   }
 
   async start(): Promise<void> {
-    this.__observability.logger.info('Starting service');
+    ObservabilityManager.logger().info('Starting service');
   }
 }
