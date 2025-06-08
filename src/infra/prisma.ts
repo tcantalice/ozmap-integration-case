81f815a865eb22ec66b0f79aabe95870d014f7a0
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-08 23:56:45 +0000

    feat(infra): Implementar finalizador de conexão do prisma

--- src/infra/prisma.ts
+++ src/infra/prisma.ts
@@ -3,3 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default prisma;
+
+export const closeConnection = async () => {
+  await prisma.$disconnect();
+};
