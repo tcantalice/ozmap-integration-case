5a7f0c223a276d23ef06190d9d82aaffbbed6cf3
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-08 01:01:46 +0000

    refactor(importer): Reetruturação e ajuste de tipagem

--- src/importer/isp-importer.ts
+++ src/importer/isp-importer.ts
@@ -1,29 +1,43 @@
-import { Box } from '../entities/box.entity';
+import SourceData from '../data/source.data';
+import Box from '../entities/box.entity';
 import Cable from '../entities/cable.entity';
 import Customer from '../entities/customer.entity';
 import DropCable from '../entities/drop-cable.entity';
 import { Location } from '../entities/vo/location';
 import ISPServiceGateway from './contracts/isp-service-gateway';
-import ISPIntegrationData, {
-  ISPBoxData,
-  ISPCableData,
-  ISPCustomerData,
-  ISPDropCableData,
-} from './isp-integration-data';
+import {
+  type ISPBoxData,
+  type ISPCableData,
+  type ISPCustomerData,
+  type ISPDropCableData,
+} from './data';
 
 export default class ISPImporter {
   constructor(private readonly gateway: ISPServiceGateway) {}
 
-  async import(): Promise<void> {
+  async import(): Promise<SourceData> {
     const data = await this.fetchData();
 
-    const boxes = this.convertISPFormatToBoxes(data.boxes);
-    const cables = this.convertISPFormatToCables(data.cables, boxes);
-    const dropCables = this.convertISPFormatToDropCables(data.dropCables, boxes);
-    const customers = this.convertISPFormatToCustomers(data.customers, dropCables);
+    const boxes = this.convertISPFormatToBoxes(data.boxes),
+      cables = this.convertISPFormatToCables(data.cables, boxes),
+      dropCables = this.convertISPFormatToDropCables(data.dropCables, boxes),
+      customers = this.convertISPFormatToCustomers(data.customers, dropCables);
+
+    return {
+      boxes,
+      cables,
+      dropCables,
+      customers,
+    };
   }
 
-  private async fetchData(): Promise<ISPIntegrationData> {
+  private async fetchData(): Promise<{
+    boxes: ISPBoxData[];
+    cables: ISPCableData[];
+    dropCables: ISPDropCableData[];
+    customers: ISPCustomerData[];
+  }> {
+    // TODO: Refatorar implementação para garantir segurança na importação
     return Promise.all([
       this.gateway.getBoxes(),
       this.gateway.getCustomers(),
