471fdac9b9a2c28bccc0c3c8ede4a1ca0ddd58a7
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-08 12:37:31 +0000

    refactor: Reestrutura bootstrap e shutdown

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,35 @@ import { load as loadConfig } from './config/loader';
 import ObservabilityManager from './observability/manager';
 import Service from './service';
 
-function bootstrap(): Service {
+async function shutdownGracefully(service: Service, exitCode = 0) {
+  try {
+    await service.shutdown();
+  } catch (error) {
+  } finally {
+    process.exit(exitCode);
+  }
+}
+
+function registerListeners(service: Service) {
+  process.on('SIGINT', () => shutdownGracefully(service, 0));
+  process.on('SIGTERM', () => shutdownGracefully(service, 0));
+}
+
+async function bootstrap() {
   const config = loadConfig();
 
   ObservabilityManager.init(config);
 
-  return new Service(config);
+  const service = new Service(config);
+
+  registerListeners(service);
+
+  try {
+    await service.run();
+  } catch (error) {
+    ObservabilityManager.logger().error('Uncaught error in bootstrap', { error });
+    await shutdownGracefully(service, 1);
+  }
 }
 
-async () => bootstrap().start();
+bootstrap();
