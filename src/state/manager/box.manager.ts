8662108681fe0e3c1a320a8527063468d606046b
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-08 20:12:52 +0000

    refactor(state/manager): Reestrutura implementação

--- src/state/manager/box.manager.ts
+++ src/state/manager/box.manager.ts
@@ -1,37 +1,55 @@
+import { State } from '../../data/state.data';
+import { Sync } from '../../data/sync.data';
 import Box from '../../entities/box.entity';
+import { SyncStatus } from '../../enums/sync-status.enum';
 import Manager from '../contracts/manager';
-import { State } from '../contracts/state';
 import Storage from '../contracts/storage';
+import StorageState from '../data/storage-state.data';
 
 export default class BoxManager implements Manager<Box> {
   constructor(private boxStorage: Storage<Box>) {}
 
   async handleState(entity: Box): Promise<State<Box>> {
-    const state: State<Box> = {
-      value: entity,
-      isNew: false,
-      hasChanges: false,
+    const result: State<Box> = {
+      data: entity,
+      operation: 'ignored',
     };
 
-    const boxSaved = await this.boxStorage.findById(entity.id);
+    const savedState: StorageState<Box> | null = await this.boxStorage.findById(entity.id);
 
-    if (boxSaved && boxSaved.hasDifference(entity)) {
+    if (savedState) {
+      if (savedState.data.hasDifference(entity)) {
+        this.registerState(entity);
+
+        result.operation = 'updated';
+      } else {
+        result.data = savedState.data;
+        result.sync = savedState.sync
+          ? {
+              lastAttemptSync: savedState.sync.lastAttemptSync,
+              lastSyncStatus: savedState.sync.lastSyncStatus,
+              syncId: savedState.sync.syncId,
+            }
+          : result.sync;
+      }
+    } else {
       this.registerState(entity);
 
-      state.value = boxSaved;
-      state.hasChanges = true;
+      result.operation = 'created';
     }
 
-    if (!boxSaved) {
-      this.registerState(entity);
+    return result;
+  }
 
-      state.isNew = true;
-    }
-
-    return state;
+  async updateSync(entity: Box, sync: Sync): Promise<void> {
+    await this.boxStorage.saveSync(entity.id, {
+      lastAttemptSync: new Date(),
+      lastSyncStatus: sync.synchronized ? SyncStatus.SUCCESS : SyncStatus.FAIL,
+      syncId: sync.syncId,
+    });
   }
 
   private async registerState(box: Box) {
-    await this.boxStorage.save(box);
+    await this.boxStorage.saveData(box);
   }
 }
