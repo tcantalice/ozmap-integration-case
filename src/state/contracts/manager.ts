5bf43dabc9508288a88f06967f98c204df5aeee9
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-08 19:32:54 +0000

    refactor(state/contracts): Redefine assinaturas

--- src/state/contracts/manager.ts
+++ src/state/contracts/manager.ts
@@ -1,5 +1,8 @@
-import { State } from './state';
+import { State } from '../../data/state.data';
+import { Synch } from '../../data/synch.data';
 
 export default interface Manager<Entity> {
   handleState(entity: Entity): Promise<State<Entity>>;
+
+  updateState(synchData: Synch<Entity>): Promise<State<Entity>>;
 }
