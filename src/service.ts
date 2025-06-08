fccd5a966a56a95ee0ee69e9815a34a842b19b6b
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-08 11:37:10 +0000

    feat(service): Adiciona controller de integração e gerenciador de observabilidade

--- src/service.ts
+++ src/service.ts
@@ -1,9 +1,18 @@
 import { Config } from './config/types';
+import IntegrationController from './core/integration.controller';
+import ObservabilityManager from './observability/manager';
 
 export default class Service {
-  constructor(config: Config) {}
+  private readonly controller: IntegrationController;
+
+  constructor(
+    config: Config,
+    private readonly __observability: ObservabilityManager,
+  ) {
+    this.controller = new IntegrationController();
+  }
 
   async start(): Promise<void> {
-    //
+    this.__observability.logger.info('Starting service');
   }
 }
