7d63a53c045e3920b007e4cf1c37e8d4df47dd7e
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-06-09 11:16:46 +0000

    refactor(importer): Reajusta importações e conversões

--- src/importer/isp-importer.ts
+++ src/importer/isp-importer.ts
@@ -4,6 +4,8 @@ import Cable from '../entities/cable.entity';
 import Customer from '../entities/customer.entity';
 import DropCable from '../entities/drop-cable.entity';
 import Location from '../entities/vo/location';
+import { Logger } from '../observability/contracts/logger';
+import ObservabilityProvider from '../observability/provider';
 import ISPServiceGateway from './contracts/isp-service-gateway';
 import {
   type ISPBoxData,
@@ -13,22 +15,28 @@ import {
 } from './data';
 
 export default class ISPImporter {
-  constructor(private readonly gateway: ISPServiceGateway) {}
+  private readonly logger: Logger;
+
+  constructor(private readonly gateway: ISPServiceGateway) {
+    this.logger = ObservabilityProvider.logger();
+  }
 
   async import(): Promise<SourceData> {
     const data = await this.fetchData();
+
+    this.logger.debug('Imported data', {
+      boxes: data.boxes.length,
+      cables: data.cables.length,
+      dropCables: data.dropCables.length,
+      customers: data.customers.length,
+    });
 
     const boxes = this.convertISPFormatToBoxes(data.boxes),
       cables = this.convertISPFormatToCables(data.cables, boxes),
       dropCables = this.convertISPFormatToDropCables(data.dropCables, boxes),
       customers = this.convertISPFormatToCustomers(data.customers, dropCables);
 
-    return {
-      boxes,
-      cables,
-      dropCables,
-      customers,
-    };
+    return { boxes, cables, dropCables, customers };
   }
 
   private async fetchData(): Promise<{
@@ -53,24 +61,30 @@ export default class ISPImporter {
 
   private convertISPFormatToBoxes(data: ISPBoxData[]): Box[] {
     return data.map(
-      (box: ISPBoxData) => new Box(box.id, box.name, box.type, new Location(box.lat, box.lng)),
+      (box: ISPBoxData) =>
+        new Box(Number(box.id), box.name, box.type, new Location(box.lat, box.lng)),
     );
   }
 
   private convertISPFormatToCables(data: ISPCableData[], boxes: Box[]): Cable[] {
-    return data.map((cable: ISPCableData) => {
+    return data.map((data: ISPCableData) => {
       const [connectionA, connectionB] = boxes.filter((box: Box) =>
-        cable.boxes_connected.includes(box.id),
+        data.boxes_connected.includes(box.id),
       );
 
-      return new Cable(cable.id, cable.name, cable.capacity, connectionA, connectionB);
+      const cable = new Cable(Number(data.id), data.name, Number(data.capacity), data.type);
+
+      cable.connectA(connectionA);
+      cable.connectB(connectionB);
+
+      return cable;
     });
   }
 
   private convertISPFormatToDropCables(data: ISPDropCableData[], boxes: Box[]): DropCable[] {
     return data.map(
       (cable: ISPDropCableData) =>
-        new DropCable(cable.id, cable.name, boxes.find((box) => box.id === cable.box_id)!),
+        new DropCable(Number(cable.id), cable.name, boxes.find((box) => box.id === cable.box_id)!),
     );
   }
 
@@ -79,7 +93,12 @@ export default class ISPImporter {
     dropCables: DropCable[],
   ): Customer[] {
     return data.map((customer: ISPCustomerData) => {
-      const converted = new Customer(customer.id, customer.code, customer.name, customer.address),
+      const converted = new Customer(
+          Number(customer.id),
+          customer.code,
+          customer.name,
+          customer.address,
+        ),
         dropCable = dropCables.find((cable) => cable.box.id === customer.box_id)!;
 
       converted.connectedBy(dropCable);
